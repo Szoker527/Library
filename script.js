6b43e3933132eac00487e1798451c257a0243baf
szoker527 <szoker527@gmail.com>
2022-11-19 18:19:26 +0100

    Work on layout

--- script.js
+++ script.js
@@ -48,42 +48,57 @@ function addBookToLibrary(title, author, pages, isread) {
   // do stuff here
 }
 
-function showBookArray(myLibrary) {
-
-}
-
-testButton.addEventListener("click", () => {
-    // const newBook = document.createElement("div");
-    // const newTitle = document.createElement("div");
-    // const newAuthor = document.createElement("div");
-    // const newPages = document.createElement("div");
-    // const newIsRead = document.createElement("div");
+formButton.addEventListener("click", () => {
     let i = 0
     myLibrary.forEach(book => {
         console.log(book)
         createBook(book)
     })
-    // newBook.appendChild(newTitle)
-    // newBook.appendChild(newAuthor)
-    // newBook.appendChild(newPages)
-    // newBook.appendChild(newIsRead)
-    // displayBook.appendChild(newBook)
 })
 
 function createBook(book) {
     const newBook = document.createElement("div");
-    newBook.classList.add("added-book");
     const newTitle = document.createElement("div");
     const newAuthor = document.createElement("div");
     const newPages = document.createElement("div");
     const newIsRead = document.createElement("div");
+    newBook.classList.add("added-book");
+    
     newTitle.textContent = book.title;
     newAuthor.textContent = book.author;
     newPages.textContent = book.pages;
     newIsRead.textContent = book.isread;
+
     newBook.appendChild(newTitle)
     newBook.appendChild(newAuthor)
     newBook.appendChild(newPages)
     newBook.appendChild(newIsRead)
     displayBook.appendChild(newBook)
+}
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("open-modal");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.classList.add("modal-flex");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.classList.remove("modal-flex");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.classList.remove("modal-flex");
+  }
 }
