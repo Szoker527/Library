c151eb4b2bb13d9f7e684397b3473311c94fc34f
szoker527 <szoker527@gmail.com>
2022-12-16 17:38:56 +0100

    Add ESLint and prettier to format files

--- script.js
+++ script.js
@@ -7,75 +7,70 @@ const array = [];
 
 let title;
 let author;
-let pages; 
+let pages;
 let isread;
 
-    formButton.addEventListener("click", () => console.log(inputBook))
+formButton.addEventListener("click", () => console.log(inputBook));
 
-    formButton.addEventListener("click", () => {
-        inputBook.forEach(input => {
-            if (input.id === "title") {
-                title = input.value
-            }
-            else if (input.id === "author") {
-                author = input.value
-            }
-            else if (input.id === "pages") {
-                pages = input.value
-            }
-            else if (input.id === "readit") {
-                isread = input.value
-            }
-            console.log(input.value)
-            })
-            addBookToLibrary(title, author, pages, isread);
-    })
-
+formButton.addEventListener("click", () => {
+  inputBook.forEach((input) => {
+    if (input.id === "title") {
+      title = input.value;
+    } else if (input.id === "author") {
+      author = input.value;
+    } else if (input.id === "pages") {
+      pages = input.value;
+    } else if (input.id === "readit") {
+      isread = input.value;
+    }
+    console.log(input.value);
+  });
+  addBookToLibrary(title, author, pages, isread);
+});
 
 const myLibrary = [];
 
 function Book(title, author, pages, isread) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isread = isread
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isread = isread;
   // the constructor...
 }
 
 function addBookToLibrary(title, author, pages, isread) {
-    myLibrary.push( new Book(title, author, pages, isread))
-    console.log(myLibrary)
+  myLibrary.push(new Book(title, author, pages, isread));
+  console.log(myLibrary);
   // do stuff here
 }
 
 formButton.addEventListener("click", () => {
-    let i = 0
-    myLibrary.forEach(book => {
-        console.log(book)
-        createBook(book)
-    })
-})
+  let i = 0;
+  myLibrary.forEach((book) => {
+    console.log(book);
+    createBook(book);
+  });
+});
 
 function createBook(book) {
-    const newBook = document.createElement("div");
-    const newTitle = document.createElement("div");
-    const newAuthor = document.createElement("div");
-    const newPages = document.createElement("div");
-    const newIsRead = document.createElement("div");
-    newBook.classList.add("added-book");
-    
-    newTitle.textContent = book.title;
-    newAuthor.textContent = book.author;
-    newPages.textContent = book.pages;
-    newIsRead.textContent = book.isread;
+  const newBook = document.createElement("div");
+  const newTitle = document.createElement("div");
+  const newAuthor = document.createElement("div");
+  const newPages = document.createElement("div");
+  const newIsRead = document.createElement("div");
+  newBook.classList.add("added-book");
 
-    newBook.appendChild(newTitle)
-    newBook.appendChild(newAuthor)
-    newBook.appendChild(newPages)
-    newBook.appendChild(newIsRead)
-    displayBook.appendChild(newBook)
+  newTitle.textContent = book.title;
+  newAuthor.textContent = book.author;
+  newPages.textContent = book.pages;
+  newIsRead.textContent = book.isread;
+
+  newBook.appendChild(newTitle);
+  newBook.appendChild(newAuthor);
+  newBook.appendChild(newPages);
+  newBook.appendChild(newIsRead);
+  displayBook.appendChild(newBook);
 }
-
 
 // Get the modal
 const modal = document.getElementById("myModal");
@@ -87,18 +82,18 @@ const btn = document.getElementById("open-modal");
 const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.classList.add("modal-flex");
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.classList.remove("modal-flex");
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.classList.remove("modal-flex");
   }
-}
+};
